8a67490ecba1e87f428c066de291b8964ba47b9d
yofredev <yofre.developer@gmail.com>
2025-06-17 19:41:49 -0300

    fix: weather fetch error

--- src/api/wheather_api.js
+++ src/api/wheather_api.js
@@ -2,18 +2,6 @@
 const API_KEY = `2da6f5c51cefd8e4d5bca21dbc5e3a9d`;
 
 export async function getWheather(lat,lon){
-<<<<<<< HEAD
-    const wheatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-    const response = await fetch(wheatherApiUrl)
-    
-        if( !response.ok )throw new Error(`bad response`)
-        const data = await response.json()
-            
-        if(data.length === 0 ) throw new Error(`Ciudad no encontrada`)
-        const {weather,main,wind} = data
-        return {weather, main,wind}
-=======
-    
     try{const wheatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
     const response = await fetch(wheatherApiUrl)
     const data = await response.json()
@@ -24,5 +12,4 @@ export async function getWheather(lat,lon){
 }
         
 
->>>>>>> 1821652 (refactor(geocode-api): change use of promises to async/await)
 }
