4e9af8a473f0fc5548ae2cbc0940a0a67eb7198a
yofredev <yofre.developer@gmail.com>
2025-06-16 22:52:04 -0300

    refactor/weather-fetch

--- src/api/wheather_api.js
+++ src/api/wheather_api.js
@@ -1,22 +1,14 @@
 //@ts-nocheck
 const API_KEY = `2da6f5c51cefd8e4d5bca21dbc5e3a9d`;
 
-export function getWheather(lat,lon){
+export async function getWheather(lat,lon){
     const wheatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-    return fetch(wheatherApiUrl)
-    .then( response =>{
-        if( response.ok ){
-            console.log({status:response.status});
-            return response.json()
-        }else{
-            console.log(response);
+    const response = await fetch(wheatherApiUrl)
+    
+        if( !response.ok )throw new Error(`bad response`)
+        const data = await response.json()
             
-        }
-    })
-    .then(data => {
         if(data.length === 0 ) throw new Error(`Ciudad no encontrada`)
         const {weather,main,wind} = data
         return {weather, main,wind}
-        } )
-
 }
